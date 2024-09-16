72307ff3549657664eaece371af006278fcbaf37
Elexic <gift228338@gmail.com>
2024-09-16 12:11:31 +0200

    Update 1.0.2
    Own application buttons were created.

--- src/Remembvoc/MainWindow.xaml.cs
+++ src/Remembvoc/MainWindow.xaml.cs
@@ -21,4 +21,19 @@ public partial class MainWindow : Window
 
         base.OnClosed(e);
     }
+
+    private void MWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
 }
