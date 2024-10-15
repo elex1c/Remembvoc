b67ea2db6b292bc41f4ab8b74a2a0bff3344a9b5
Elexic <89549464+elex1c@users.noreply.github.com>
2024-10-15 10:25:20 +0200

    Update 1.0.8.4
    Word translation was added for words table and interface.
    TODO: Create new migration and update the database.

--- src/Remembvoc/Models/ApplicationModels/Words.cs
+++ src/Remembvoc/Models/ApplicationModels/Words.cs
@@ -7,6 +7,8 @@ public class Words
     public int Id { get; set; }
     [Required]
     public string Phrase { get; set; }
+    [Required]
+    public string Translation { get; set; }
     public int LanguageId { get; set; }
     public Languages Language { get; set; }
 }
--- src/Remembvoc/Models/Word.cs
+++ src/Remembvoc/Models/Word.cs
@@ -5,11 +5,12 @@ namespace Remembvoc.Models;
 public class Word
 {
     public string Phrase { get; set; }
+    public string Translation { get; set; }
     public Languages Language { get; set; }
 
     public static explicit operator Word(Words word)
     {
         Enum.TryParse(word.Language.ShortForm, out Languages language);
-        return new Word { Phrase = word.Phrase, Language = language };
+        return new Word { Phrase = word.Phrase, Translation = word.Translation, Language = language };
     }
 }
